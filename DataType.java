f6b4837920aa42e53781e08fdd32b36f6f6cbd1c
anjali2001kaushik <107063467+anjali2001kaushik@users.noreply.github.com>
2022-09-26 16:36:38 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,14 @@
+ import java.util.Scanner;
+class DataType{
+     public static strictfp void main(String[] args) {
+        System.out.println("I'm not java wala main");
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         float w=100.f;
+         
+         System.out.println(w);
+         System.out.println(n);
+         sc.close();
+
+    }
+}
